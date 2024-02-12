d65dd116f7435b68cf5448ceb4aeabbdb1b7fbce
Arjun <arjunnaik0402@gmail.com>
2024-02-12 20:56:31 +0530

    News API react

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
         <div className="card-body" style={{position:"relative"}}>
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} style={{padding:"3px 90px",position:"absolute",bottom:"20px"}} target="_blank" className="btn btn-sm btn-dark">Read more</a>
+            <a href={newsUrl} style={{padding:"3px 90px",position:"absolute",bottom:"20px"}} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read more</a>
         </div>
     </div>
     </>
