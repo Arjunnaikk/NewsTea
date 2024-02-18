2ca8cf6cd34594ed5b72a0e8d2ee6206f7004b3a
Arjun <arjunnaik0402@gmail.com>
2024-02-18 17:21:44 +0530

    Adding infinite scrolling and Loading bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,20 +12,34 @@ import {
 
 
 export default class App extends Component {
-  pageSize = 12
+  pageSize = 6
+  apiKey = process.env.REACT_APP_NEWS_API
+  // apiKey = "69e58ca2fb8349528f087079c7145bff"
+  state ={
+    progress:0
+  }
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
+
   render() {
     return (
       <>
       <Router>
+      <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={this.state.progress}
+      />
       <Navbar/>
       <Routes>
-        <Route exact path='/Business' element={<News key="Business" pageSize={this.pageSize} country={'us'} category={"Business"}/>}></Route>
-        <Route exact path='/Entertainment' element={<News key="Entertainment" pageSize={this.pageSize} country={'us'} category={"Entertainment"}/>}></Route>
-        <Route exact path='/General' element={<News key="General" pageSize={this.pageSize} country={'us'} category={"General"}/>}></Route>
-        <Route exact path='/Health' element={<News key="Health" pageSize={this.pageSize} country={'us'} category={"Health"}/>}></Route>
-        <Route exact path='/Science' element={<News key="Science" pageSize={this.pageSize} country={'us'} category={"Science"}/>}></Route>
-        <Route exact path='/Sports' element={<News key="Sports" pageSize={this.pageSize} country={'us'} category={"Sports"}/>}></Route>
-        <Route exact path='/Technology' element={<News key="Technology" pageSize={this.pageSize} country={'us'} category={"Technology"}/>}></Route>
+        <Route exact path='/Business' element={<News setProgress={this.setProgress} key="Business" pageSize={this.pageSize} country={'us'} apiKey={this.apiKey} category={"Business"}/>}></Route>
+        <Route exact path='/Entertainment' element={<News setProgress={this.setProgress} key="Entertainment" pageSize={this.pageSize} country={'us'} category={"Entertainment"}/>}></Route>
+        <Route exact path='/General' element={<News setProgress={this.setProgress} key="General" pageSize={this.pageSize} country={'us'} category={"General"}/>}></Route>
+        <Route exact path='/Health' element={<News setProgress={this.setProgress} key="Health" pageSize={this.pageSize} country={'us'} category={"Health"}/>}></Route>
+        <Route exact path='/Science' element={<News setProgress={this.setProgress} key="Science" pageSize={this.pageSize} country={'us'} category={"Science"}/>}></Route>
+        <Route exact path='/Sports' element={<News setProgress={this.setProgress} key="Sports" pageSize={this.pageSize} country={'us'} category={"Sports"}/>}></Route>
+        <Route exact path='/Technology' element={<News setProgress={this.setProgress} key="Technology" pageSize={this.pageSize} country={'us'} category={"Technology"}/>}></Route>
       </Routes>
       </Router>
       </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,8 +5,9 @@ export class NewsItem extends Component {
       let {title, description, imgUrl, newsUrl, author, publishedAt, source} = this.props;
       return (
     <>
-    <div className="card" style={{width: "22em",height:"28rem"}}>  <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:"1"}}>
-    {source}<span class="visually-hidden">unread messages</span></span>
+    <div className="card" style={{width: "22em",height:"28rem"}}> 
+    <div style={{display:"flex",justifyContent:"flex-end",position:'absolute',right:'0'}}><span class="badge rounded-pill bg-danger" style={{left:'90%',zIndex:"1"}}>
+    {source}<span class="visually-hidden">unread messages</span></span></div>
         <img src={imgUrl} className="card-img-top" style={{maxHeight:"195px"}} alt="..."/>
         <div className="card-body" style={{position:"relative"}}>
             <h5 className="card-title">{title}</h5>
